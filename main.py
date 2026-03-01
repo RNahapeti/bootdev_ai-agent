eb53ff226a936f9bcf26dba6e51ce6d2cdc3894e
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-02-28 21:36:39 -0800

    refactor: move code from module level to main()

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("Gemini API key is missing")
-client = genai.Client(api_key=api_key)
-
 def main():
 #    print("Hello from ai-agent!")
-    result = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("Gemini API key is missing")
+    client = genai.Client(api_key=api_key)
+
+    result = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    print("Gemini response:")
     print(result.text)
 
 
