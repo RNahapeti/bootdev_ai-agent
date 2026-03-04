53ba7157c99b0df913a344affd9bb1e1edbc4166
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-03-03 19:22:19 -0800

    refactor: break out request/response from main()
    Added a new helper function for the chat cycle and printed responses to
    live in.

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ def main():
     parser.add_argument("user_prompt", type=str, help="Add user prompt to send to Gemini - use quotes")
     args = parser.parse_args()
 
-    # Authentication - retrieve API key and initializ connection to Google servers
+    # Setup - load environment variables, validate API key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("Gemini API key is missing")
+    
+    # Prep  - Initialize AI client and convo history
     client = genai.Client(api_key=api_key)
-
-    # API request - use specific model and pass on user prompt
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # Start chat
+    generate_chat(client, messages)
+
+# API request/response cycle
+def generate_chat(client, messages):
     response = client.models.generate_content(
         model = "gemini-2.5-flash",
         contents = messages
