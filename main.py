cb3c6bb9c267bac351556d72d16a064ec111c27e
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-03-03 18:47:20 -0800

    feature: add user prompt input in command-line
    After testing with a hard-coded prompt, now we have the ability to enter
    any prompt directly as a command-line argument.

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+# Standard libraries
 import os
+import argparse
+# Third-Party libraries
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-#    print("Hello from ai-agent!")
+    # Use argparse library to get user prompt as a command-line argument
+    parser = argparse.ArgumentParser(description="Gemini code assistant")
+    parser.add_argument("user_prompt", type=str, help="Add user prompt to send to Gemini - use quotes")
+    args = parser.parse_args()
+
+    # Authentication - retrieve API key and initializ connection to Google servers
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key == None:
+    if api_key is None:
         raise RuntimeError("Gemini API key is missing")
     client = genai.Client(api_key=api_key)
 
-    result = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    # API request - use specific model and pass on user prompt
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = args.user_prompt
     )
 
+    # Check for API resquest error - did we receive data back?
+    if response.usage_metadata is None:
+        raise RuntimeError("Failed API request")
+    
+    # Output
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Gemini response:")
-    print(result.text)
+    print(response.text)
 
 
 if __name__ == "__main__":
