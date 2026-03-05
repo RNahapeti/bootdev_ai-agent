e89fc4cb2ba80634a0bf29e40b79e6777473b5ee
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-03-04 19:36:46 -0800

    Add function to test file access for AI
    Can test that project directory is accessible by running module
    test_get_files_info.py

---
+++ test_get_files_info.py
@@ -0,0 +1,10 @@
+from functions.get_files_info import get_files_info
+
+current_dir = get_files_info("calculator", ".")
+pkg_dir = get_files_info("calculator", "pkg")
+bin_dir = get_files_info("calculator", "/bin")
+prev_dir = get_files_info("calculator", "../")
+print("Result for current directory:\n" + current_dir + "\n")
+print("Result for 'pkg' directory:\n" + pkg_dir + "\n")
+print("Result for '/bin' directory:\n" + bin_dir + "\n")
+print("Result for '../' directory:\n" + prev_dir)
