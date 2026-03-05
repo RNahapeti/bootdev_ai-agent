1ca83298521c0d9219bbeead0f1e6a6fbde7f0d2
RNahapeti <55406449+RNahapeti@users.noreply.github.com>
2026-03-04 19:45:51 -0800

    Fix test module by defining function test_access

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,10 +1,15 @@
 from functions.get_files_info import get_files_info
 
-current_dir = get_files_info("calculator", ".")
-pkg_dir = get_files_info("calculator", "pkg")
-bin_dir = get_files_info("calculator", "/bin")
-prev_dir = get_files_info("calculator", "../")
-print("Result for current directory:\n" + current_dir + "\n")
-print("Result for 'pkg' directory:\n" + pkg_dir + "\n")
-print("Result for '/bin' directory:\n" + bin_dir + "\n")
-print("Result for '../' directory:\n" + prev_dir)
+def test_access():
+    current_dir = get_files_info("calculator", ".")
+    pkg_dir = get_files_info("calculator", "pkg")
+    bin_dir = get_files_info("calculator", "/bin")
+    prev_dir = get_files_info("calculator", "../")
+
+    print("Result for current directory:\n" + current_dir + "\n")
+    print("Result for 'pkg' directory:\n" + pkg_dir + "\n")
+    print("Result for '/bin' directory:\n" + bin_dir + "\n")
+    print("Result for '../' directory:\n" + prev_dir)
+
+if __name__ == "__main__":
+    test_access()
